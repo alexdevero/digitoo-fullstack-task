35d59a9da9375b05c944533dfe39423a0f2f0250
Alex Devero <deveroalex@gmail.com>
2021-02-13 14:38:32 +0100

    Cover basic cases

---
+++ server/utils/utils.js
@@ -0,0 +1,12 @@
+// Check if E-mail is Valid or not
+function isScramble(str1, str2) {
+  if (str1.length < str2.length) return false
+
+  if (str1.length === 1) return str1 === str2
+
+  if (str1 === str2) return true
+
+  return resultArr
+}
+
+module.exports = isScramble
